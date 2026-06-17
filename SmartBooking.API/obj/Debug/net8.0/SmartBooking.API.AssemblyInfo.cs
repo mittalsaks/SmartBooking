0b69d4386d5d19bd48a313bb2fbc95f41615292a
Sakshi Mittal <sakshimittal753@gmail.com>
2026-06-17 21:08:36 +0530

    Updated

--- SmartBooking.API/obj/Debug/net8.0/SmartBooking.API.AssemblyInfo.cs
+++ SmartBooking.API/obj/Debug/net8.0/SmartBooking.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartBooking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3156d9e378348fe6cb4e5ebdcc7c3d6f12aeb51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbba615f2fcaa3fb8e6628859bc1721f711f6f37")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartBooking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartBooking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
